440ad1b2440cd27638fb4f49e121e87301c2cb1d
Khan Mohd Asif Altaf <161149248+asifkhan7060@users.noreply.github.com>
2026-07-05 21:07:28 +0530

    Time: 1 ms (99.17%), Space: 42.5 MB (84.95%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,18 +1,39 @@
+// class Solution {
+//     public int digitFrequencyScore(int n) {
+
+//         int sum = 0;
+//         int x = 0;
+//         // 123
+
+//         while(n != 0){
+
+//             x = n % 10;
+//             sum = sum + x;
+//             n = n/10;
+//         }
+
+//         return sum;
+        
+//     }
+// }
+
 class Solution {
     public int digitFrequencyScore(int n) {
 
-        int sum = 0;
-        int x = 0;
-        // 123
+        int[] freq = new int[10];
 
-        while(n != 0){
-
-            x = n % 10;
-            sum = sum + x;
-            n = n/10;
+        while (n > 0) {
+            int digit = n % 10;
+            freq[digit]++;
+            n /= 10;
         }
 
-        return sum;
-        
+        int score = 0;
+
+        for (int i = 0; i < 10; i++) {
+            score += i * freq[i];
+        }
+
+        return score;
     }
 }
