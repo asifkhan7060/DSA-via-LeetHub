fe0bac2c0c74a6b8d0b94cfaff19ea4288fe02c6
Khan Mohd Asif Altaf <161149248+asifkhan7060@users.noreply.github.com>
2026-07-06 08:55:40 +0530

    Time: 1 ms (100%), Space: 42.9 MB (92.43%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+
+        // int x = num;
+
+        // for(int i =0;i<t;i++){
+
+        //     x = x+2;
+
+        // }
+        
+        // return x;
+
+        return num + 2*t;
+    }
+}
